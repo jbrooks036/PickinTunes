bb02cb5589b9fa7829ea652e75aac10d7e854811
Jo Ann Brooks <joannbrooks036@gmail.com>
2016-07-06 16:17:31 -0500

    WIP - changing Artist and Tune models

--- src/PickinTunes/Migrations/20160706210615_TuneArtistModels.Designer.cs
+++ src/PickinTunes/Migrations/20160706210615_TuneArtistModels.Designer.cs
@@ -8,9 +8,10 @@ using PickinTunes.Models;
 namespace PickinTunes.Migrations
 {
     [DbContext(typeof(PickinTunesContext))]
-    partial class PickinTunesContextModelSnapshot : ModelSnapshot
+    [Migration("20160706210615_TuneArtistModels")]
+    partial class TuneArtistModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
---
+++ src/PickinTunes/Migrations/20160706210615_TuneArtistModels.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PickinTunes.Migrations
+{
+    public partial class TuneArtistModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Artist_Tune_TuneId",
+                table: "Artist");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Artist_TuneId",
+                table: "Artist");
+
+            migrationBuilder.DropColumn(
+                name: "TuneId",
+                table: "Artist");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ArtistId",
+                table: "Tune",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tune_ArtistId",
+                table: "Tune",
+                column: "ArtistId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tune_Artist_ArtistId",
+                table: "Tune",
+                column: "ArtistId",
+                principalTable: "Artist",
+                principalColumn: "ArtistId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tune_Artist_ArtistId",
+                table: "Tune");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tune_ArtistId",
+                table: "Tune");
+
+            migrationBuilder.DropColumn(
+                name: "ArtistId",
+                table: "Tune");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TuneId",
+                table: "Artist",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artist_TuneId",
+                table: "Artist",
+                column: "TuneId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Artist_Tune_TuneId",
+                table: "Artist",
+                column: "TuneId",
+                principalTable: "Tune",
+                principalColumn: "TuneId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
